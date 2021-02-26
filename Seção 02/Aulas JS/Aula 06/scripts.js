c83f648178364129ae9264f7a6f4caa13f0a34b4
Luan <luanfnn@gmail.com>
2021-02-26 12:52:12 -0400

    Final da seção 2

---
+++ Seção 02/Aulas JS/Aula 06/scripts.js
@@ -0,0 +1,3 @@
+console.log(5 > 2);
+console.log(3 > 10);
+console.log(100 < 200);
---
+++ Seção 02/Aulas JS/Aula 08/scripts.js
@@ -0,0 +1,4 @@
+console.log(2 > 3 && 4 > 3); 
+console.log(2 > 3 || 4 > 3);
+console.log(!((3 * 8) > 200 || (4%2) < (5 * 5)));
+console.log(3 == 3 && "Luan" == "Luan");
