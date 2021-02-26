24a5880f6447b7436c88f625e2405a9a22c4df74
Luan <luanfnn@gmail.com>
2021-02-26 11:48:28 -0400

    Atualizando as aulas da seção 2

---
+++ Seção 02/Aulas JS/Aula 04/scripts.js
@@ -0,0 +1,4 @@
+console.log("Teste 1");
+console.log('Teste, 2');
+console.log(`TeStE 3`);
+console.log(4);
---
+++ Seção 02/Aulas JS/Aula 05/scripts.js
@@ -0,0 +1,4 @@
+console.log("Essa é uma String \n De duas linhas");
+console.log("The name is 'JOHN CENA'");
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+console.log("O " + "meu " + "nome " + "é " + "Luan.");
