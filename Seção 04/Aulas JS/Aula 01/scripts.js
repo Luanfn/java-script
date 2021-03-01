a1c392470e6cf65c25ec9ec3f8dccd1197530ef5
Luan <luanfnn@gmail.com>
2021-03-01 18:43:45 -0400

    Inicio da seção 4

---
+++ Seção 04/Aulas JS/Aula 01/scripts.js
@@ -0,0 +1,24 @@
+let nome = "Luan";
+
+console.log(nome);
+console.log(`O meu nome é: ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "Xaum";
+
+console.log(nome);
+
+laranjas = 1500;
+
+console.log(laranjas);
+
+laranjas = "laranjas";
+
+console.log(laranjas);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
---
+++ Seção 04/Aulas JS/Aula 03/scripts.js
@@ -0,0 +1,7 @@
+let nome1 = "teste";
+let $nome = "teste 2";
+let _nome = "teste 3";
+//let @nome = "teste";
+//let !nome = "teste";
+
+let seuNome = "teste";
